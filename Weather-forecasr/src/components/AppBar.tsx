8ec4cef6f6c71f8e5382d7d07ecbe333a0504cd1
unknown <rithwhickpraharsha@gmail.com>
2024-03-27 05:03:24 +0530

    Applyin assn

--- Weather-forecasr/src/components/AppBar.tsx
+++ Weather-forecasr/src/components/AppBar.tsx
@@ -10,7 +10,7 @@ export default function AppBar(){
             <div className="text-xl ml-3 font-semibold text-white font-serif">ApplyIn.co</div>
             <div className="flex items-center text-white font-semibold">
          <img src ={userData.picture || ''} className="rounded-full h-12 mr-4 my-2"></img>
-         <div className="text-xl font-serif  my-2">{userData.name}</div>
+         <div className="text-xl font-serif mr-3 my-2">{userData.name}</div>
          </div>
          
         </div>
